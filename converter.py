2eaf0963e22111040a9a8e7905a8a01faa283803
anuragpeshne <anurag.peshne@gmail.com>
2015-12-16 23:02:05 +0530

    get related path from OS

--- converter.py
+++ converter.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python
 import sys
+import os
 
 if len(sys.argv) < 2 or ord(sys.argv[1]) > 127:
     print 'usage: converter.py character'
 
-f = open('./ascii')
+f = open(os.path.dirname(__file__)+ '/ascii')
 charCode = ord(sys.argv[1])
 
 f.seek(charCode * ((4 * 8) + 8))
