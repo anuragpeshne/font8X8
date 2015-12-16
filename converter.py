066994d6c472b968737f70156ab90174bd12cc74
Anurag Peshne <anurag.peshne@gmail.com>
2015-12-16 16:06:05 +0530

    python version working

---
+++ converter.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+import sys
+
+if len(sys.argv) < 2 or ord(sys.argv[1]) > 127:
+    print 'usage: converter.py character'
+
+f = open('./ascii')
+charCode = ord(sys.argv[1])
+
+f.seek(charCode * ((4 * 8) + 8))
+#                  |______|  |_|
+#                   4 char    whitespace
+
+row = f.read((4 * 8) + (1 * 8))
+matrix = row.split()
+matrix = map(lambda x: bin(int(x, 16))[2:].zfill(8)[::-1], matrix)
+print '\n'.join(matrix)
